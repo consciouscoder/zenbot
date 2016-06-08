5f126d04859ec2e1f4b139003d46edf78b8c8437
DANILO LADENDORF <dladendorf@Ds-MacBook-Pro.local>
2016-06-07 18:44:04 -0600

    deploy 1

--- googlebot.js
+++ googlebot.js
@@ -122,7 +122,7 @@ module.exports = function(query, callback) {
 			var $ = cheerio.load(body),
 				links = $(".r a");
 
-			console.log('inside googlebot.js! 2')
+			console.log('inside googlebot.js! 2', links)
 
 			links.each(function (i, link) {
 				// get the href attribute of each link
